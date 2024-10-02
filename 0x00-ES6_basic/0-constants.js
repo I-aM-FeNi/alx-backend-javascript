bbaa639131a41fa6ba1636f48fae301dbf1b094c
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-02 23:19:31 +0000

    Removed comments to meet code requirements

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,21 +1,15 @@
-// taskFirst function uses 'const' to declare a constant variable 'task'
 export function taskFirst() {
-  // 'task' is a constant variable and cannot be reassigned
   const task = 'I prefer const when I can.';
-  return task; // returns the string assigned to 'task'
+  return task;
 }
 
-// getLast function returns a simple string
 export function getLast() {
-  return ' is okay'; // returns ' is okay'
+  return ' is okay';
 }
 
-// taskNext function uses 'let' to declare a variable 'combination'
 export function taskNext() {
-  // 'combination' is declared with 'let' because its value will change
   let combination = 'But sometimes let';
-  // 'combination' is modified by appending the string returned from getLast
-  combination += getLast(); // 'combination' becomes 'But sometimes let is okay'
+  combination += getLast();
 
-  return combination; // returns the final value of 'combination'
+  return combination;
 }

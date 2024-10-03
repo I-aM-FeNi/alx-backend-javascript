f7b87fbf255b16d46988a546061fc1451a370034
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-03 01:29:18 +0000

    Removed comments

--- 0x00-ES6_basic/_tests_/1-block-scoped.test.js
+++ 0x00-ES6_basic/_tests_/1-block-scoped.test.js
@@ -1,7 +1,7 @@
 import taskBlock from '../1-block-scoped.js';
 
 describe('taskBlock', () => {
-  it('should return [true, false] when true is passed', () => {
+  it('should return [false, true] when false is passed', () => {
     expect(taskBlock(true)).toEqual([true, false]);
   });
 

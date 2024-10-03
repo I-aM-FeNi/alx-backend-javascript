7da142da9f1544b2a57519605682356bcb64bc3b
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-03 03:24:58 +0000

    Worked on Jest Test

--- 0x00-ES6_basic/_tests_/1-block-scoped.test.js
+++ 0x00-ES6_basic/_tests_/1-block-scoped.test.js
@@ -1,8 +1,8 @@
 import taskBlock from '../1-block-scoped.js';
 
 describe('taskBlock', () => {
-  it('should return [false, true] when false is passed', () => {
-    expect(taskBlock(true)).toEqual([true, false]);
+  it('should return [false, true] when true is passed', () => {
+    expect(taskBlock(true)).toEqual([false, true]);
   });
 
   it('should return [false, true] when false is passed', () => {

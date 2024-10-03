b5274bb3619ca588ad5b8feab2b9c04cd98d9113
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-03 08:10:33 +0000

    Created test files

---
+++ 0x01-ES6_promise/_tests_/0-promise.test.js
@@ -0,0 +1,7 @@
+import getResponseFromAPI from '../0-promise';
+
+test('getResponseFromAPI should return a Promise', () => {
+  const response = getResponseFromAPI();
+  expect(response).toBeInstanceOf(Promise);
+});
+

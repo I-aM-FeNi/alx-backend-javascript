5d06ffd68c4bf227371b32d80a6eca6c902d29f3
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-03 00:26:07 +0000

    Tested for lint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false; // Use 'const' to prevent reassignment
-  const task2 = true; // Use 'const' to prevent reassignment
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // This 'task' is block-scoped to this 'if' block
-    const task2 = false; // This 'task2' is also block-scoped to this 'if' block
+    task = true;
+    task2 = false;
   }
 
-  return [task, task2]; // Return the original 'task' and 'task2'
+  return [task, task2];
 }

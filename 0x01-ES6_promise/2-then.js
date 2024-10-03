267d4fe372bc71688b482172772dc2b46fea6caa
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-03 08:52:55 +0000

    Created the 2-then.test.js file with the test file

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((result) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;

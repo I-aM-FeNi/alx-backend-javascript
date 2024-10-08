f2000f59512c476dec3402f8e39d00c012782bee
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-08 11:20:58 +0000

    Created the 10-update_uniq_items.js and test files

---
+++ 0x03-ES6_data_manipulation/_tests_/10-update_uniq_items.test.js
@@ -0,0 +1,18 @@
+import updateUniqueItems from '../10-update_uniq_items.js';
+import groceriesList from '../9-groceries_list.js';
+
+describe('updateUniqueItems', () => {
+  it('should update the quantity to 100 for items with initial quantity of 1', () => {
+    const map = groceriesList();
+    updateUniqueItems(map);
+    expect(map.get('Pasta')).toBe(100);
+    expect(map.get('Rice')).toBe(100);
+  });
+
+  it('should throw an error if the argument is not a map', () => {
+    expect(() => {
+      updateUniqueItems({});
+    }).toThrow('Cannot process');
+  });
+});
+

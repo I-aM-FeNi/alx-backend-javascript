a6f316432635f46c116dafc205572e148663b81d
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-09 16:12:56 +0000

    task_1/js/main.ts, task_1/webpack.config.js, task_1/tsconfig.json, task_1/package.json

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// task_1/js/main.ts
+
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any; // Index signature to allow additional properties
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
